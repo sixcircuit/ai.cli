d52d2e90642721a97027c764db62306130eaa9c6
Kendrick Taylor <kendrick@circuitsix.com>
2023-10-16 14:19:01 -0700

    initial commit

---
+++ config.example.js
@@ -0,0 +1,9 @@
+// copy this file to "./config.js" in the root of the project.
+module.exports = {
+   open_ai: {
+      api: {
+         // your api key goes here, as a string.
+         key: null
+      }
+   }
+};
