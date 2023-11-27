7c8664704c5f26783081c3e306d859c9bda52726
Kendrick Taylor <kendrick@circuitsix.com>
2023-11-27 00:41:47 -0500

    refactor a ton of stuff, shell out to an editor, support wrapping text (working on streaming wrap and gutter)

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 // copy this file to "./config.js" in the root of the project.
 module.exports = {
-   open_ai: {
+   openai: {
       api: {
          // your api key goes here, as a string.
          key: null
