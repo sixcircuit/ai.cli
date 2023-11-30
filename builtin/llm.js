eb6cdeb9c24bfa3e529440abcf3dde5ceaade89a
Kendrick Taylor <kendrick@circuitsix.com>
2023-11-29 23:34:21 -0500

    mid refactor

---
+++ builtin/llm.js
@@ -0,0 +1,26 @@
+
+const llm = { name: "llm" };
+
+llm.run = async function(){
+
+}
+
+llm.help = function(no_exit){
+
+   _.line("usage: llm <model | alias | cont> [<helper_name> [prompt]]");
+   _.line("helpers: ", _helpers.help());
+   _.line("aliases: ", pretty_obj(ai.models.help().aliases));
+   _.line("models: ", pretty_obj(ai.models.help().models));
+   _.line("helpers: ", _helpers.help());
+
+   if(no_exit){ return; }
+
+   process.exit(1); 
+};
+
+
+
+
+module.exports = function({ ai }){
+   ai.register(llm);
+};
