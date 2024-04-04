51211f4730c0d538cd663cacf719be875f5f86be
Kendrick Taylor <kendrick@circuitsix.com>
2024-04-03 17:28:01 -0700

    format stats, fix bug with parsing backtick code blocks

--- builtin/helpers.js
+++ builtin/helpers.js
@@ -8,7 +8,7 @@ function helpers_class(){
    };
 }
 
-helpers_class.prototype.get = function(key){ return this._helpers[key]; };
+helpers_class.prototype.get = function(key){ return _.resolve_alias(this._helpers, key); };
 helpers_class.prototype.add = function(helpers){ this._helpers = _.merge(this._helpers, helpers); };
 helpers_class.prototype.help = function(){ return JSON.stringify(Object.keys(this._helpers)); }
 
--- builtin/llm.js
+++ builtin/llm.js
@@ -28,21 +28,67 @@ module.exports = function({ _, ai, config }){
 
    function show_stats({ stats }){
 
-      const table = {
-         "took": (stats.took + "ms"),
-         "tokens (input)": _.to_k(stats.input.token_length),
-         "tokens (output)": _.to_k(stats.output.token_length),
-         "price (input)": _.format.dollars(stats.input.price),
-         "price (output)": _.format.dollars(stats.output.price),
-         "price (total)": _.format.dollars(stats.price),
-         "model (series)": stats.model.series,
-         "model (version)": stats.model.version,
-      };
+      // const table = {
+      //    // "model": stats.model.version + "\n",
+      //    // "model (series)": stats.model.series,
+      //    "tokens in": _.to_k(stats.input.token_length),
+      //    "price in": _.format.dollars(stats.input.price) + "\n",
+      //    "tokens out": _.to_k(stats.output.token_length),
+      //    "price out": _.format.dollars(stats.output.price) + "\n",
+      //    "price": _.format.dollars(stats.price) + "\n",
+      //    "took": (stats.took + "ms"),
+      // };
 
-      const chunk = _.format.obj_table(table);
+      // const chunk = _.format.obj_table(table);
 
-      _console.turn({ role: "stats" });
-      _console.flush({ chunk, new_line: true });
+      // _console.turn({ color: "stats", label: "model: " });
+      // _console.flush({ chunk: stats.model.version, new_line: true });
+
+      const token_table = _.format.obj_table({
+         // "took": (stats.took + "ms"),
+         // "model": stats.model.version,
+         // "endl_0": null,
+         "in": _.to_k(stats.input.token_length) + ` tok @ ${_.format.dollars(stats.input.price)}`,
+         "out": _.to_k(stats.output.token_length) + ` tok @ ${_.format.dollars(stats.output.price)}`,
+         "endl_1": null,
+         "total": _.to_k(stats.token_length) + ` tok @ ${ _.format.dollars(stats.price) }`,
+      });
+
+      _console.label({ color: "stats", label: "took: " });
+      _console.flush({ chunk: (stats.took + "ms") });
+
+      _console.label({ color: "stats", label: "model: " });
+      _console.flush({ chunk: stats.model.version });
+
+      // _console.label({ color: "stats", label: "tokens: " });
+      // _console.flush({ chunk: `${_.to_k(stats.input.token_length)} / ${_.to_k(stats.output.token_length)}` });
+
+      // _console.label({ color: "stats", label: "price: " });
+      // _console.flush({ chunk: `${_.format.dollars(stats.input.price)} / ${_.format.dollars(stats.output.price)}` });
+
+      _console.label({ color: "stats", label: "in: " });
+      _console.flush({ chunk: `${_.format.dollars(stats.input.price)} (${_.to_k(stats.input.token_length)} tokens)` });
+
+      _console.label({ color: "stats", label: "out: " });
+      _console.flush({ chunk: `${_.format.dollars(stats.output.price)} (${_.to_k(stats.output.token_length)} tokens)` });
+
+      _console.label({ color: "stats", label: "total: " });
+      _console.flush({ color: null, chunk: `${_.format.dollars(stats.price)} (${_.to_k(stats.token_length)} tokens)` });
+
+      // _console.turn({ color: "stats", label: "stats: " });
+      // _console.flush({ chunk: token_table, new_line: true });
+
+      // const price_table = _.format.obj_table({
+      //    "in": ,
+      //    "out":
+      //    "total":
+      // });
+
+      // _console.turn({ color: "stats", label: "price: " });
+      // _console.flush({ chunk: price_table, new_line: true });
+
+      // _console.turn({ role: "stats" });
+      // _console.flush({ chunk, new_line: true });
    }
 
    function make_stats({ start, end, input, output, model }){
@@ -62,6 +108,7 @@ module.exports = function({ _, ai, config }){
          input: _stat(input_stats, "in"),
          output: _stat(output_stats, "out"),
          price: (input_stats.price_in + output_stats.price_out),
+         token_length: (input_stats.token_length + output_stats.token_length),
          model: { version: model.version(), series: model.series() }
       };
 
@@ -133,10 +180,11 @@ module.exports = function({ _, ai, config }){
 
       save_history({ input, output, stats, model });
 
-      await copy_to_clipboard({ output });
-
       show_warnings({ model, stats });
 
+      await copy_to_clipboard({ output });
+
+      _console.ensure_new_line();
       _console.new_line();
 
       _console.flush();
@@ -148,7 +196,7 @@ module.exports = function({ _, ai, config }){
       let model_name = argv.shift();
       let prompt = "";
 
-      if(!model_name || model_name === "help" || model_name === "--help"){ return help(); }
+      if(!model_name || model_name === "help" || model_name === "--help"){ return llm.help(); }
 
       if(model_name === "cont"){ _.fatal("cont not implemented"); }
 
@@ -156,7 +204,7 @@ module.exports = function({ _, ai, config }){
 
       if(!model){
          _.print("unknown model type: ", model_name, " you must pick a model or alias in the list below.");
-         return help();
+         return llm.help();
       }
 
       if(argv.length){
@@ -165,7 +213,9 @@ module.exports = function({ _, ai, config }){
          prompt = argv.join(" ").trim();
          if(!helper){
             _.print(`unknown helper "`, helper_name, `": need one in the list below.`);
-            return help();
+            return llm.help();
+         }else{
+            _.print("helper: ", helper);
          }
       }
 
@@ -228,8 +278,8 @@ module.exports = function({ _, ai, config }){
 
       _.print("usage: llm <model | alias | cont> [<helper_name> [prompt]]");
       _.print("helpers: ", _helpers.help());
-      _.print("aliases: ", _.format.pretty_obj(ai.models.help().aliases));
-      _.print("models: ", _.format.pretty_obj(ai.models.help().models));
+      _.print("aliases: ", _.format.pretty_obj(ai.models().help().aliases));
+      _.print("models: ", _.format.pretty_obj(ai.models().help().models));
       _.print("helpers: ", _helpers.help());
 
       if(no_exit){ return; }
